cd9e62937ce993a086e635891b389cf1f72e0aa2
Samuel Gonzalez <xame2005@gmail.com>
2021-07-24 21:32:54 -0500

    [CREATED] Music fest v 1.0

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,35 @@
-console.log("Listo!");
+document.addEventListener("DOMContentLoaded", function () {
+  scrollNav();
+
+  navegacionFija();
+});
+
+function navegacionFija() {
+  const barra = document.querySelector(".header");
+  //Registrar el elemento a observar
+  const observer = new IntersectionObserver(function (entries) {
+    if (entries[0].isIntersecting) {
+      barra.classList.remove("fijo");
+    } else {
+      barra.classList.add("fijo");
+    }
+  });
+  //Elemento a observar
+  observer.observe(document.querySelector(".sobre-festival"));
+}
+
+function scrollNav() {
+  const enlaces = document.querySelectorAll(".navegacion-principal a");
+
+  enlaces.forEach(function (enlace) {
+    enlace.addEventListener("clik", function (e) {
+      e.preventDefault();
+
+      const seccion = document.querySelector(e.target.attributes.href.value);
+
+      seccion.scrollIntoView({
+        behavior: "smooth",
+      });
+    });
+  });
+}
